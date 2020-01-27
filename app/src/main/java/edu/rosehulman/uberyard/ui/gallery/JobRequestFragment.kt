fd1dc3e1f643c13219f575cea429d1f0fd894559
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2020-01-26 22:25:02 -0500

    Added more elements to a job request fragment

--- app/src/main/java/edu/rosehulman/uberyard/ui/gallery/JobRequestFragment.kt
+++ app/src/main/java/edu/rosehulman/uberyard/ui/gallery/JobRequestFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -11,6 +13,7 @@ import androidx.lifecycle.ViewModelProviders
 import edu.rosehulman.uberyard.R
 
 class GalleryFragment : Fragment() {
+    val list : Array<String> = arrayOf("Lawn Maintenence", "Tree Removal","Watering System Repairs" , "Other")
 
 
     override fun onCreateView(
@@ -20,7 +23,10 @@ class GalleryFragment : Fragment() {
     ): View? {
 
         val root = inflater.inflate(R.layout.fragment_job_request, container, false)
-
+        val spinner = root.findViewById<Spinner>(R.id.job_type)
+        var adapter : ArrayAdapter<String> = ArrayAdapter(activity!!.applicationContext ,  android.R.layout.simple_spinner_item, list)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
         return root
     }
 }
