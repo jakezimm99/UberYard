af29ac0001cd2682bcf514db49c52f964b14d027
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2020-02-17 19:35:44 -0500

    Completely reworked fragments, now can pass data along via the navigation items

--- app/src/main/java/edu/rosehulman/uberyard/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/uberyard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -21,10 +22,17 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import edu.rosehulman.uberyard.ui.billing.BillingFragment
 import edu.rosehulman.uberyard.ui.home.HomeFragment
+import edu.rosehulman.uberyard.ui.jobhistory.JobHistoryFragment
+import edu.rosehulman.uberyard.ui.jobrequest.JobRequestFragment
+import edu.rosehulman.uberyard.ui.jobstatuses.JobStatusesFragment
 import edu.rosehulman.uberyard.ui.login.LoginFragment
+import edu.rosehulman.uberyard.ui.settings.SettingsFragment
 
 // Developed by Jake Zimmerman and Zach Thelen
 
@@ -39,17 +47,17 @@ class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedList
 
     private fun launchLogin() {
         val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
         startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-            RC_SIGN_IN
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN
         )
     }
 
@@ -58,6 +66,8 @@ class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedList
 
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,34 +80,108 @@ class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedList
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
 //        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
 
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_login, R.id.nav_home, R.id.nav_job_request, R.id.nav_job_history,
-                R.id.nav_job_statuses, R.id.nav_settings, R.id.nav_billing
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        Log.d("Uber", "About to call the listener method")
-
-
-        initializeListeners()
+        var fragment: Fragment = LoginFragment()
         authListener = FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
             val user = auth.currentUser
             Log.d("Uber", "$user")
             if (user != null) {
                 Log.d("Uber", "Main activity")
+                fragment = HomeFragment()
             } else {
                 Log.d("Uber", "Log in activity")
-                navController.navigate(R.id.action_a_to_b)
+                fragment = LoginFragment()
             }
         }
+
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_login -> {
+                    fragment = LoginFragment()
+                    val ft = supportFragmentManager.beginTransaction()
+                    ft.replace(R.id.flContent, fragment)
+                    ft.commit()
+
+                    true
+                }
+                R.id.nav_home -> {
+                    fragment = HomeFragment()
+                    if (auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                R.id.nav_job_request -> {
+                    fragment = JobRequestFragment()
+                    if(auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                R.id.nav_job_history -> {
+                    fragment = JobHistoryFragment()
+                    if(auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                R.id.nav_job_statuses -> {
+                    fragment = JobStatusesFragment()
+                    if(auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                R.id.nav_settings -> {
+                    fragment = SettingsFragment()
+                    if(auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                R.id.nav_billing -> {
+                    fragment = BillingFragment()
+                    if(auth.currentUser != null) {
+                        val ft = supportFragmentManager.beginTransaction()
+                        ft.replace(R.id.flContent, fragment)
+                        ft.commit()
+                    }
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+
+//        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+//        appBarConfiguration = AppBarConfiguration(
+//                setOf(
+//                        R.id.nav_login, R.id.nav_home, R.id.nav_job_request, R.id.nav_job_history,
+//                        R.id.nav_job_statuses, R.id.nav_settings, R.id.nav_billing
+//                ), drawerLayout
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+        Log.d("Uber", "About to call the listener method")
+
+
+
     }
 
     override fun onStart() {
@@ -109,32 +193,23 @@ class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedList
         super.onStop()
         auth.removeAuthStateListener(authListener)
     }
-
-    fun initializeListeners() {
-
-    }
-
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         if (requestCode == 17) {
             if (permissions.size == 1 &&
-                permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
             ) {
-//                val home = supportFragmentManager.findFragmentById(R.id.nav_home) as HomeFragment
-//                home.map!!.setMyLocationEnabled(true);
+                val home = supportFragmentManager.findFragmentById(R.id.nav_home) as HomeFragment
+                home.map!!.setMyLocationEnabled(true);
             } else {
                 // Permission was denied. Display an error message.
             }
         }
     }
-
-
-
-
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -152,7 +227,11 @@ class MainActivity : AppCompatActivity(), LoginFragment.OnLoginButtonPressedList
                 auth.signOut()
                 true
             }
-
+            R.id.nav_button -> {
+                if (!drawerLayout.isDrawerOpen(GravityCompat.START)) drawerLayout.openDrawer(GravityCompat.START);
+                else drawerLayout.closeDrawer(GravityCompat.END);
+                true
+            }
 
 
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/edu/rosehulman/uberyard/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/rosehulman/uberyard/ui/home/HomeFragment.kt
@@ -35,6 +35,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
         map?.uiSettings?.isMyLocationButtonEnabled = true
 
+
     }
 
 
